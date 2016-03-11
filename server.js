164e2189ffb1e903957ec02c407dfb79a3f45b94
Claudenir Machado <claudenirfreitas.cf@gmail.com>
2016-03-10 21:54:32 -0300

    teste travis

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-var http = require("http");
-var express = require("express");
-var app  = require('./config/express')();
+"use strict";
+
+const http = require("http"),
+    express = require("express"),
+    app = require('./config/express')();
 
 require("./config/passport")();
 require("./config/database")("mongodb://localhost/contatooh");
 
 http.createServer(app).listen(
         app.get("port"),
-        function () {
-            console.log("Express Server escutando na porta %s", app.get("port"));
+        () => {
+            console.log(`Express Server escutando na porta ${ app.get("port") }`);
         }
 );
